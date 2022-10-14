d77f53c8abf036d889a261c32ea762c858cfb7ed
Unpei2 <r.ho5@share.epsb.ca>
2022-10-14 14:50:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-
-
-
+let box1 = document.getElementById("b1")
+let box2 = document.getElementById("b2")
+let box3 = document.getElementById("b3")
+let box4 = document.getElementById("b4")
+let box5 = document.getElementById("b5")
+let box6 = document.getElementById("b6")
 
 
 randomlist = []
+for (let x = 0;x < 6 ;x++){
+    
+    randomintoarray(randomRGB())
+    console.log(randomlist)
+    console.log(randomintoarray())
 
-for (let x = 0;x <6 ;x++){
-    randomlist += randomRGB()
 }
-document.getElementById("b1").style.backgrouncolor = "orange"
-document.getElementById("b2").innerHTML = randomlist[1]
-document.getElementById("b3").innerHTML = randomlist[2]
-document.getElementById("b4").innerHTML = randomlist[3]
-document.getElementById("b5").innerHTML = randomlist[4]
-document.getElementById("b6").innerHTML = randomlist[5]
-console.log(randomlist)
+
+function randomintoarray(rgb){
+    return {
+        rgb
+    }
+}
+
+
+
+
